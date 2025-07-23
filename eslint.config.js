9ebe74f0a9d36d9eb4aef96db8fcc8d5930d14bb
gtgavint <gtgavint@gmail.com>
2025-07-24 03:52:30 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+// eslint-plugin-security-node plugin + eslint-plugin-no-unsanitized plugin
+import pluginJs from '@eslint/js';
+import pluginSecurity from 'eslint-plugin-security';
+import pluginSecurityNode from 'eslint-plugin-security-node';
+import pluginNoUnsanitized from 'eslint-plugin-no-unsanitized';
+import globals from 'globals';
 
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.mocha
+      }
+    },
+    plugins: {
+      security: pluginSecurity,
+      'security-node': pluginSecurityNode,
+      'no-unsanitized': pluginNoUnsanitized
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      'security/detect-eval-with-expression': 'error',
+      'security-node/detect-insecure-randomness': 'warn',
+      'no-unsanitized/method': 'warn',
+      'no-unsanitized/property': 'warn'
+    }
+  }
+];
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-]);
